c383953c3b88ca6710e346c7eb44a45b7a5ba92f
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-08-27 19:50:36 +0200

    modified the main application class file

--- src/main/java/com/app/blog/BlogApplication.java
+++ src/main/java/com/app/blog/BlogApplication.java
@@ -1,13 +1,15 @@
 package com.app.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class BlogApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
+		new SpringApplicationBuilder(BlogApplication.class)
+				.properties("spring.config.use-legacy-processing=true")
+				.run(args);
 	}
-
 }
