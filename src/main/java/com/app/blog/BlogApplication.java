59b95bca0ada58ad62fbc70f6e5d8d9e8013464a
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-08-27 22:35:11 +0200

    added env on the Dockerfile

--- src/main/java/com/app/blog/BlogApplication.java
+++ src/main/java/com/app/blog/BlogApplication.java
@@ -10,12 +10,4 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
-	@PostConstruct
-	public void logDbConfig() {
-		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
-	}
-
-
 }
