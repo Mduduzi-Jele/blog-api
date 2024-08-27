91ab7c27b5bc2c773ce19ed61f515231f645ec11
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-08-27 20:12:35 +0200

    Added logDbConfig

--- src/main/java/com/app/blog/BlogApplication.java
+++ src/main/java/com/app/blog/BlogApplication.java
@@ -1,15 +1,21 @@
 package com.app.blog;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class BlogApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(BlogApplication.class)
-				.properties("spring.config.use-legacy-processing=true")
-				.run(args);
+		SpringApplication.run(BlogApplication.class, args);
 	}
+
+	@PostConstruct
+	public void logDbConfig() {
+		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
+	}
+
+
 }
